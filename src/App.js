25d129db3615e4e96b323ba2cf006e273c663664
Panagiotis Panagi <ppanagi@dynopia.com>
2017-07-05 20:45:58 +0300

    Move code from old next.js repo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import R from 'ramda';
+import { lifecycle } from 'recompose';
+import { Container, Image, Card } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import base from './app/api/rebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { posts } = this.props;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Card.Group>
+            {
+              R.map(post => (
+                <Card key={post.oldId + post.categoryChild} href={post.permalink} target="_blank">
+                  <Image
+                    src="http://via.placeholder.com/400x200"
+                    alt={post.title}
+                  />
+                  <Card.Content>
+                    <Card.Header>
+                      {post.title}
+                    </Card.Header>
+                    <Card.Meta>
+                      {post.oldId}
+                    </Card.Meta>
+                    <Card.Description>
+                      <div dangerouslySetInnerHTML={{ __html: post.body }} />
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    {post.phone}
+                  </Card.Content>
+                </Card>
+              ), posts)
+            }
+          </Card.Group>
+        </Container>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object),
+};
+
+App.defaultProps = {
+  posts: [],
+};
+
+export default R.compose(
+  lifecycle({
+    componentWillMount() {
+      base.bindToState('posts', {
+        context: this,
+        state: 'posts',
+        asArray: true,
+      });
+    },
+  }),
+)(App);
---
+++ src/app/api/rebase.js
@@ -0,0 +1,20 @@
+import Rebase from 're-base';
+import firebase from 'firebase/app';
+import database from 'firebase/database';
+
+// FIXME: This is to avoid the error:
+// `Firebase: Firebase App named '[DEFAULT]' already exists (app/duplicate-app).`
+// Find a cleaner way to fix this.
+const name = Math.random().toString(36).substring(7);
+
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyDqwM_WYZLPllRbSqbxpYhWuklXhl4-gAU',
+  authDomain: 'test-768f2.firebaseapp.com',
+  databaseURL: 'https://test-768f2.firebaseio.com',
+  projectId: 'test-768f2',
+}, name);
+
+const db = database(app);
+const base = Rebase.createClass(db);
+
+export default base;
