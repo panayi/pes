dd5f57f2ddeaa881412b68c997a8aa7f9f2312d9
Panagiotis Panagi <ppanagi@dynopia.com>
2017-08-30 21:16:02 +0300

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Provider as RebassProvider } from 'rebass';
 import { ConnectedRouter } from 'react-router-redux';
+import 'normalize.css/normalize.css';
 import createStore from './lib/store/createStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+
 
 const store = createStore(window.__INITIAL_STATE__); // eslint-disable-line no-underscore-dangle
 
