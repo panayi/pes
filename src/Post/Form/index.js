104e2e59d3da3d6be8550f9c4a0d25d8ede68cd5
Panagiotis Panagi <ppanagi@dynopia.com>
2017-09-05 22:41:24 +0300

    Add categories select to Post form

--- src/Post/Form/index.js
+++ src/Post/Form/index.js
@@ -1,21 +1,26 @@
 /* @flow */
 import React from 'react';
+import R from 'ramda';
 import { Flex, Button, Input, Label } from 'rebass';
 import { Control, Form } from 'react-redux-form';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
 import UploadFile from '../../lib/components/UploadFile';
 import { jpeg, png, gif } from '../../lib/constants/filetypes';
 
 export const initialState = {
   title: '',
   body: '',
+  category: '',
 };
 
 type Props = {
   onSubmit: Function,
   filesPath: string,
+  categories: Array<Category>,
 };
 
-export default (props: Props) => (
+const PostForm = (props: Props) => (
   <Form
     model="forms.post"
     onSubmit={props.onSubmit}
@@ -41,9 +46,38 @@ export default (props: Props) => (
         filesPath={props.filesPath}
         acceptedTypes={[jpeg, png, gif]}
       />
+      <Label htmlFor="forms.post.category">
+        Category:
+      </Label>
+      <Control.select
+        model="forms.post.category"
+        id="forms.posts.category"
+      >
+        {R.map(category => (
+          <option
+            key={category.name}
+            value={category.name}
+          >
+            {category.name}
+          </option>
+        ), props.categories)}
+      </Control.select>
     </Flex>
     <Button type="submit">
       Post
     </Button>
   </Form>
 );
+
+PostForm.defaultProps = {
+  categories: [],
+};
+
+export default R.compose(
+  firebaseConnect([
+    'categories',
+  ]),
+  connect((state: Object): Object => ({
+    categories: state.firebase.data.categories,
+  })),
+)(PostForm);
