ea9688115e1057fcb2135d728e2b1cb76238e22d
Panagiotis Panagi <ppanagi@dynopia.com>
2017-09-09 12:40:56 +0300

    Fix lint errors

--- src/home/index.js
+++ src/home/index.js
@@ -1,63 +1,57 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+/* @flow */
+import React from 'react';
 import R from 'ramda';
 import { Flex, Box, Column, Card, BackgroundImage, Subhead, Badge, Text, Small } from 'rebass';
 import withCategories from '../Categories/withCategoriesHoc';
 import withPosts from '../Posts/withPostsHoc';
 
-export class Home extends Component {
-  static propTypes = {
-    categories: PropTypes.arrayOf(PropTypes.shape({})),
-    posts: PropTypes.arrayOf(PropTypes.shape({})),
-  };
+type Props = {
+  categories: Array<Category>,
+  posts: Array<Post>,
+};
 
-  render() {
-    const { categories, posts } = this.props;
-
-    return (
-      <Flex mt={4} ml={3}>
-        <Box w="300px">
-          <Text bold>Categories</Text>
-          {R.map(category => (
-            <Text key={category.name}>
-              {category.name}
-            </Text>
-          ), categories)}
-        </Box>
-        <Flex wrap>
-          {
-            R.map(post => (
-              <Column
-                key={post.oldId + post.categoryChild}
-                w="300px"
-              >
-                <Card
-                  href={post.permalink}
-                  target="_blank"
-                >
-                  <BackgroundImage
-                    src="http://via.placeholder.com/400x200"
-                    alt={post.title}
-                  />
-                  <Subhead p={2}>
-                    {post.title}
-                    <Badge>
-                      {post.oldId}
-                    </Badge>
-                  </Subhead>
-                  <Small>
-                    {post.phone}
-                  </Small>
-                  <Text dangerouslySetInnerHTML={{ __html: post.body }} />
-                </Card>
-              </Column>
-            ), posts)
-          }
-        </Flex>
-      </Flex>
-    );
-  }
-}
+const Home = (props: Props) => (
+  <Flex mt={4} ml={3}>
+    <Box w="300px">
+      <Text bold>Categories</Text>
+      {R.map(category => (
+        <Text key={category.name}>
+          {category.name}
+        </Text>
+      ), props.categories)}
+    </Box>
+    <Flex wrap>
+      {
+        R.map(post => (
+          <Column
+            key={post.oldId + post.categoryChild}
+            w="300px"
+          >
+            <Card
+              href={post.permalink}
+              target="_blank"
+            >
+              <BackgroundImage
+                src="http://via.placeholder.com/400x200"
+                alt={post.title}
+              />
+              <Subhead p={2}>
+                {post.title}
+                <Badge>
+                  {post.oldId}
+                </Badge>
+              </Subhead>
+              <Small>
+                {post.phone}
+              </Small>
+              <Text dangerouslySetInnerHTML={{ __html: post.body }} />
+            </Card>
+          </Column>
+        ), props.posts)
+      }
+    </Flex>
+  </Flex>
+);
 
 export default R.compose(
   withCategories,
