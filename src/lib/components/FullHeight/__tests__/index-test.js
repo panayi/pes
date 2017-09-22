511b9fc3b51c6d10d20ef3f9e9ee50048748ff7d
Panagiotis Panagi <ppanagi@dynopia.com>
2017-09-22 20:11:11 +0300

    Add first snapshot testing

--- src/lib/components/FullHeight/__tests__/index-test.js
+++ src/lib/components/FullHeight/__tests__/index-test.js
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme';
 import FullHeight from '../index';
 
 describe('[Component] FullHeight', () => {
-  it('should render as a div', () => {
-    const wrapper = shallow(<FullHeight />);
-    expect(wrapper).toHaveTagName('div');
+  it('should render correctly', () => {
+    const wrapper = shallow(
+      <FullHeight>
+        <header>I am a header</header>
+        <article>I am an article</article>
+      </FullHeight>
+    );
+    expect(wrapper).toMatchSnapshot();
   });
 });
