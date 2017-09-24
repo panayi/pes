951a0aa886fb39b1272b07d14cf3538cb9ef38a7
Panagiotis Panagi <ppanagi@dynopia.com>
2017-09-24 21:20:26 +0300

    * Mock store and firebase for unit testing
    * Add tests for Login/index.js

---
+++ src/auth/Login/__tests__/index.test.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import R from 'ramda';
+import { mount } from 'enzyme';
+import ConnectedLogin, { Login } from '../index';
+
+describe('Login', () => {
+  it('should render correctly', () => {
+    const LoginComponent = () => (
+      <div>
+        I live within login component
+      </div>
+    );
+    const login = noop;
+    const wrapper = mount(
+      <Login
+        component={LoginComponent}
+        firebase={{ login }}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should pass correct props to LoginComponent', () => {
+    const LoginComponent = R.always(<div />);
+    const login = R.always('firebase.login');
+    const authError = {
+      foo: 'bar',
+    };
+    const wrapper = mount(
+      <Login
+        component={LoginComponent}
+        firebase={{ login }}
+        authError={authError}
+      />
+    );
+    const loginComponent = wrapper.find(LoginComponent);
+    expect(loginComponent.prop('login')).toBe(login);
+    expect(loginComponent.prop('onSuccess')).toBe(wrapper.instance().handleSuccess);
+    expect(loginComponent.prop('onError')).toBe(wrapper.instance().handleError);
+    expect(loginComponent.prop('authError')).toBe(authError);
+  });
+
+  it('should call maybeMergeAnonymousProfile on success', () => {
+    const LoginComponent = R.always(<div />);
+    const login = noop;
+    const mockMaybeMergeAnonymousProfile = jest.fn();
+    const wrapper = mount(
+      <Login
+        component={LoginComponent}
+        firebase={{ login }}
+        maybeMergeAnonymousProfile={mockMaybeMergeAnonymousProfile}
+      />
+    );
+
+    expect(mockMaybeMergeAnonymousProfile.mock.calls.length).toBe(0);
+    wrapper.instance().handleSuccess();
+    expect(mockMaybeMergeAnonymousProfile.mock.calls.length).toBe(1);
+  });
+
+  describe('connected', () => {
+    it('should connect correct props', () => {
+      const LoginComponent = R.always(<div />);
+      const state = {
+        firebase: {
+          authError: {
+            foo: 'bar',
+          },
+        },
+      };
+      const { component, store } = withProvider(
+        <ConnectedLogin component={LoginComponent} />,
+        state,
+      );
+      const wrapper = mount(component);
+      const wrappedComponent = wrapper.find(Login);
+
+      expect(R.is(Object, wrappedComponent.prop('firebase'))).toBe(true);
+      expect(wrappedComponent.prop('authError')).toEqual({ foo: 'bar' });
+      expect(R.is(Function, wrappedComponent.prop('maybeMergeAnonymousProfile'))).toBe(true);
+    });
+  });
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,12 +1,87 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import * as Firebase from 'firebase';
 import { Provider } from 'react-redux';
+import { applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import FirebaseServer from 'firebase-server';
+import configureMockStore from 'redux-mock-store';
+import detectPort from 'detect-port';
 import 'jest-enzyme';
 
 global.noop = () => {};
 
-global.withProvider = (children, store) => (
-  <Provider store={store}>
-    {children}
-  </Provider>
-);
+// ------------------------------------
+// Setup store and Firebase
+// ------------------------------------
+
+global.Firebase = Firebase;
+
+const firebaseConfig = {
+  apiKey: 'asdf', // placeholder
+  authDomain: 'asdf', // placeholder
+  databaseURL: 'ws://127.0.1:5000',
+  storageBucket: 'asdf', // placeholder
+  messagingSenderId: 'asdf', // placeholder
+};
+
+detectPort(5000)
+  .then((port) => {
+    if (port === 5000) {
+      new FirebaseServer(5000, 'localhost.firebaseio.test', { // eslint-disable-line no-new
+        users: {
+          Iq5b0qK2NtgggT6U3bU6iZRGyma2: {
+            displayName: 'Tester',
+          },
+        },
+      });
+    }
+
+    // Swallow firebase reinitialize error (useful when using watch)
+    try {
+      Firebase.initializeApp(firebaseConfig);
+    } catch (err) {
+      // do nothing
+    }
+  });
+
+
+global.firebase = Object.defineProperty(Firebase, '_', {
+  value: {
+    watchers: {},
+    authUid: null,
+    config: Object.assign(
+      {},
+      firebaseConfig,
+      {
+        userProfile: 'users',
+        enableRedirectHandling: false, // disabled due to lack of http/https
+      },
+    ),
+  },
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
+
+// ------------------------------------
+// Helpers
+// ------------------------------------
+
+global.withProvider = (children, state = {}) => {
+  const store = configureMockStore(compose(
+    reactReduxFirebase(Firebase, { userProfile: 'users', enableRedirectHandling: false }),
+    applyMiddleware(thunk.withExtraArgument(getFirebase)),
+  ))(state);
+  store.firebase = global.firebase;
+
+  return {
+    component: (
+      <Provider store={store}>
+        {children}
+      </Provider>
+    ),
+    store,
+  };
+};
