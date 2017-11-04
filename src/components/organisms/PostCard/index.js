fa771445c45af72750875bb053ccd7c7ad55414e
Panagiotis Panagi <ppanagi@dynopia.com>
2017-11-05 00:54:09 +0200

    Fix masonry layout messing up when changing category

--- src/components/organisms/PostCard/index.js
+++ src/components/organisms/PostCard/index.js
@@ -10,9 +10,14 @@ type Props = {
   classes: Object,
 };
 
-const DEFAULT_HEIGHT = 200;
+const DEFAULT_MEDIA_HEIGHT = 200;
+const HEADER_HEIGHT = 60;
+const CONTENT_HEIGHT = 30;
 
 const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
   post: {
     textDecoration: 'none',
   },
@@ -29,59 +34,79 @@ const styles = theme => ({
     padding: theme.spacing.unit,
   },
   header: {
+    padding: theme.spacing.unit,
+    height: HEADER_HEIGHT,
+    boxSizing: 'border-box',
+    overflow: 'hidden',
     textAlign: 'center',
   },
+  content: {
+    padding: 0,
+    height: CONTENT_HEIGHT,
+    boxSizing: 'border-box',
+    overflow: 'hidden',
+  },
   headerTitle: {
-    fontSize: theme.typography.title.fontSize,
+    fontSize: theme.typography.subheading.fontSize,
   },
 });
 
-const PostCard = ({ post, width, classes }: Props) => (
-  <Card
-    className={classes.post}
-    classes={{
-      root: classes.postPaperRoot,
-    }}
-    style={{ width }}
-    elevation={1}
-    component={Link}
-    to={`/i/${post.objectID}`}
-  >
-    <CardMedia
-      className={classes.media}
-      title={post.title}
-      {...getMediaProps(post, { defaultHeight: DEFAULT_HEIGHT })}
+const PostCard = ({ post, width, classes }: Props) => {
+  const mediaProps = getMediaProps(post, { defaultHeight: DEFAULT_MEDIA_HEIGHT });
+  const totalHeight = mediaProps.style.height + HEADER_HEIGHT + CONTENT_HEIGHT;
+
+  return (
+    <div
+      className={classes.root}
+      style={{ width: `${width}px`, height: `${totalHeight}px` }}
     >
-      {
-        post.price && post.price > 0 &&
-          <Typography
-            className={classes.price}
-            color="accent"
-            type="title"
-          >
-            €&nbsp;{post.price}
-          </Typography>
-      }
-    </CardMedia>
-    <CardHeader
-      className={classes.header}
-      classes={{
-        title: classes.headerTitle,
-      }}
-      title={post.title}
-    />
-    <CardContent>
-      {
-        post.address &&
-          <Typography
-            type="caption"
-            align="center"
-          >
-            {post.address}
-          </Typography>
-      }
-    </CardContent>
-  </Card>
-);
+      <Card
+        className={classes.post}
+        classes={{
+          root: classes.postPaperRoot,
+        }}
+        style={{ width }}
+        elevation={1}
+        component={Link}
+        to={`/i/${post.objectID}`}
+      >
+        <CardMedia
+          className={classes.media}
+          title={post.title}
+          {...getMediaProps(post, { defaultHeight: DEFAULT_MEDIA_HEIGHT })}
+        >
+          {
+            post.price && post.price > 0 &&
+              <Typography
+                className={classes.price}
+                color="accent"
+                type="title"
+              >
+                €&nbsp;{post.price}
+              </Typography>
+          }
+        </CardMedia>
+        <CardHeader
+          className={classes.header}
+          classes={{
+            title: classes.headerTitle,
+          }}
+          title={post.title}
+        />
+        <CardContent className={classes.content}>
+          {
+            post.address &&
+              <Typography
+                type="caption"
+                align="center"
+              >
+                {post.address}
+              </Typography>
+          }
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 export default withStyles(styles)(PostCard);
