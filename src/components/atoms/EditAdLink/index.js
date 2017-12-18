5f4a93084fd738817051d4389f50e4209d2384d9
Panagiotis Panagi <ppanagi@dynopia.com>
2017-12-18 20:20:09 +0200

    Use history.replace instead of link for EditAdLink

--- src/components/atoms/EditAdLink/index.js
+++ src/components/atoms/EditAdLink/index.js
@@ -1,24 +1,32 @@
 /* @flow */
 import React from 'react';
 import * as R from 'ramda';
+import { connect } from 'react-redux';
+import { replace as _replace } from 'react-router-redux';
+import { IconButton } from 'material-ui';
 import propsSelector from 'utils/propsSelector';
 import requirePropToRender from 'components/hocs/requirePropToRender';
 import withUserWithId from 'components/hocs/withUserWithId';
-import Link from 'components/molecules/Link';
 
 type Props = {
   ad: Ad,
   adId: string,
   children: React$Node,
+  replace: Function,
 };
 
-const EditAdLink = ({ adId, children, ...rest }: Props) => (
-  <Link.icon {...rest} to={`/i/${adId}/edit`}>
+const EditAdLink = ({ adId, children, replace, ...rest }: Props) => (
+  <IconButton {...rest} onClick={() => replace(`/i/${adId}/edit`)}>
     {children}
-  </Link.icon>
+  </IconButton>
 );
+
+const mapDispatchToProps = {
+  replace: _replace,
+};
 
 export default R.compose(
   requirePropToRender('ad'),
   withUserWithId(R.compose(R.path(['ad', 'user']), propsSelector)),
+  connect(null, mapDispatchToProps),
 )(EditAdLink);
