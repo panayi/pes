e5514d927d2dbe412b56eb6444b0499c9cd3b20a
Panagiotis Panagi <ppanagi@dynopia.com>
2018-02-03 23:26:20 +0200

    Temp disable types in packages/config to fix build issue

--- packages/config/src/env.js
+++ packages/config/src/env.js
@@ -1,19 +1,20 @@
 /* @flow */
-export type Env = {|
-  REACT_APP_FIREBASE_API_KEY: string,
-  REACT_APP_FIREBASE_PROJECT_ID: string,
-  REACT_APP_FIREBASE_FUNCTIONS_BASE_URL: string,
-  REACT_APP_ALGOLIA_APP_ID: string,
-  REACT_APP_ALGOLIA_SEARCH_KEY: string,
-  REACT_APP_IMGIX_HOST: string,
-  REACT_APP_GOOGLE_APIS_KEY: string,
-  GOOGLE_APIS_SERVER_KEY: ?string,
-  ALGOLIA_API_KEY: ?string,
-  LEGACY_PESPOSA_BASE_URL: ?string,
-  LEGACY_PESPOSA_TOKEN: ?string,
-|};
+// export type Env = {|
+//   REACT_APP_FIREBASE_API_KEY: string,
+//   REACT_APP_FIREBASE_PROJECT_ID: string,
+//   REACT_APP_FIREBASE_FUNCTIONS_BASE_URL: string,
+//   REACT_APP_ALGOLIA_APP_ID: string,
+//   REACT_APP_ALGOLIA_SEARCH_KEY: string,
+//   REACT_APP_IMGIX_HOST: string,
+//   REACT_APP_GOOGLE_APIS_KEY: string,
+//   GOOGLE_APIS_SERVER_KEY: ?string,
+//   ALGOLIA_API_KEY: ?string,
+//   LEGACY_PESPOSA_BASE_URL: ?string,
+//   LEGACY_PESPOSA_TOKEN: ?string,
+// |};
 
-const env = ((process.env: any): Env);
+// const env = ((process.env: any): Env);
+const { env } = process;
 
 const output = {
   firebaseApiKey: env.REACT_APP_FIREBASE_API_KEY,
