d6e4662e49596996bc5c269d6c967fd5c61709b6
Panagiotis Panagi <ppanagi@dynopia.com>
2018-04-02 11:39:08 +0300

    Transpile packages/core in razzle build

--- tools/config/razzle.web.js
+++ tools/config/razzle.web.js
@@ -1,12 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const BUILD_PATH = path.join(__dirname, '..', '..', 'build', 'web');
 
 module.exports = {
   modify(config, { target, dev }) {
     const appConfig = config; // stay immutable here
+
+    appConfig.module.rules[0].include = [
+      appConfig.module.rules[0].include,
+      path.join(__dirname, '..', '..', 'packages/core/src'),
+    ];
+    appConfig.module.rules[0].use[0].options.baseConfig = require.resolve(
+      '../../.eslintrc.js',
+    );
+    appConfig.module.rules[1].include = [
+      ...appConfig.module.rules[1].include,
+      path.join(__dirname, '..', '..', 'packages/core/src'),
+    ];
 
     if (target === 'node' && !dev) {
       appConfig.entry = path.join(
@@ -21,32 +32,6 @@ module.exports = {
       appConfig.output.path = path.join(BUILD_PATH, 'server');
       appConfig.output.filename = 'server.bundle.js';
       appConfig.output.libraryTarget = 'commonjs2';
-    }
-
-    if (target === 'web' && !dev) {
-      appConfig.plugins = [
-        ...appConfig.plugins,
-        // TODO: remove once we upgrade razzle/after.js
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            compress: {
-              warnings: false,
-              // Disabled because of an issue with Uglify breaking seemingly valid code:
-              // https://github.com/facebookincubator/create-react-app/issues/2376
-              // Pending further investigation:
-              // https://github.com/mishoo/UglifyJS2/issues/2011
-              comparisons: false,
-            },
-            mangle: {
-              safari10: true,
-            },
-            output: {
-              comments: false,
-            },
-          },
-          sourceMap: true,
-        }),
-      ];
     }
 
     if (target === 'node') {
