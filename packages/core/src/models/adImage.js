c3c82ebcc1f2b5cb904fd56dcc5f75cda18b2791
Panagiotis Panagi <ppanagi@dynopia.com>
2018-04-12 00:36:05 +0300

    Fix bug

--- packages/core/src/models/adImage.js
+++ packages/core/src/models/adImage.js
@@ -23,14 +23,7 @@ export const setAdImages = async (images: { [id: ID]: Image }, adId: ID) => {
 };
 
 export const setDimensions = async (imageSnapshot): Promise<boolean> => {
-  // Only set dimensions when downloadURL has changed
-  if (!imageSnapshot.child('downloadURL').changed()) {
-    return false;
-  }
-
   const image = imageSnapshot.val();
   const dimensions = await getImageDimensions(image.downloadURL);
-  await imageSnapshot.ref.set(R.merge(image, { dimensions }));
-
-  return true;
+  return imageSnapshot.ref.set(R.merge(image, { dimensions }));
 };
