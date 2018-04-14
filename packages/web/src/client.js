e576e22a64c9cd8960a22089e9d0d639cfcf81ae
Panagiotis Panagi <ppanagi@dynopia.com>
2018-04-14 19:43:36 +0300

    Use hotjar everywhere

--- packages/web/src/client.js
+++ packages/web/src/client.js
@@ -14,6 +14,7 @@ import configureStore from 'store/configureStore';
 import { constants as searchConstants } from 'store/search';
 import { selectors as responsiveSelectors } from 'store/responsive';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
+import HotjarProvider from 'components/HotjarProvider/HotjarProvider';
 import SearchProvider from 'modules/Search/Provider/Provider';
 import routes from 'routes';
 
@@ -59,6 +60,7 @@ ensureReady(routes).then(data => {
               </MuiThemeProvider>
             </JssProvider>
           </SearchProvider>
+          <HotjarProvider />
         </ErrorBoundary>
       </Provider>
     </BrowserRouter>,
--- packages/web/src/pages/beta/index.js
+++ packages/web/src/pages/beta/index.js
@@ -156,22 +156,6 @@ class Beta extends React.Component {
   componentDidMount() {
     if (env.firebaseProject === 'pesposa-production') {
       /* eslint-disable */
-
-      // Hotjar Tracking Code for beta.pesposa.com/beta
-      (function(h, o, t, j, a, r) {
-        h.hj =
-          h.hj ||
-          function() {
-            (h.hj.q = h.hj.q || []).push(arguments);
-          };
-        h._hjSettings = { hjid: 845878, hjsv: 6 };
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script');
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-
       // Facebook pixel tracking code
       !(function(f, b, e, v, n, t, s) {
         if (f.fbq) return;
@@ -232,6 +216,7 @@ class Beta extends React.Component {
     const { logout, openModal } = this.props;
     await logout();
     openModal('login', {
+      title: 'Create an account to enter the new Pesposa',
       disablePhone: true,
       onSuccess: this.handleLoginSuccess,
     });
