36fe837e25b8ae4528a14e1c52547e194a182191
Panagiotis Panagi <ppanagi@dynopia.com>
2018-04-15 10:52:36 +0300

    Enable hotjar only on pesposa-production

--- packages/web/src/components/HotjarProvider/HotjarProvider.js
+++ packages/web/src/components/HotjarProvider/HotjarProvider.js
@@ -1,22 +1,25 @@
 import { Component } from 'react';
+import env from '@pesposa/core/src/config/env';
 
 export class HotjarProvider extends Component {
   componentDidMount() {
-    /* eslint-disable */
-    (function(h, o, t, j, a, r) {
-      h.hj =
-        h.hj ||
-        function() {
-          (h.hj.q = h.hj.q || []).push(arguments);
-        };
-      h._hjSettings = { hjid: 845878, hjsv: 6 };
-      a = o.getElementsByTagName('head')[0];
-      r = o.createElement('script');
-      r.async = 1;
-      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-      a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-    /* eslint-enable */
+    if (env.firebaseProject === 'pesposa-production') {
+      /* eslint-disable */
+      (function(h, o, t, j, a, r) {
+        h.hj =
+          h.hj ||
+          function() {
+            (h.hj.q = h.hj.q || []).push(arguments);
+          };
+        h._hjSettings = { hjid: 845878, hjsv: 6 };
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script');
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+      /* eslint-enable */
+    }
   }
   render() {
     return null;
