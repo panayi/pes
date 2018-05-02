d6420a1351ac016e6fff8006b3c3b07670ea3d2c
Panagiotis Panagi <ppanagi@dynopia.com>
2018-05-02 11:32:56 +0300

    Fix tracking users on mixpanel

--- packages/web/src/modules/Mixpanel/TrackGlobalEvents/TrackGlobalEvents.js
+++ packages/web/src/modules/Mixpanel/TrackGlobalEvents/TrackGlobalEvents.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import * as R from 'ramda';
 import { isNilOrEmpty } from 'ramda-adjunct';
+import { createSelector, createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import propsChanged from '@pesposa/core/src/utils/propsChanged';
 import mixpanelService, { track } from 'services/mixpanel';
+import { selectors as authSelectors } from 'store/firebase/auth';
+import { selectors as userInfoselectors } from 'store/userInfo';
+import { selectors as profileSelectors } from 'store/firebase/profile';
 
 class TrackGlobalEvents extends React.Component {
   componentDidMount() {
@@ -69,4 +74,26 @@ class TrackGlobalEvents extends React.Component {
   }
 }
 
-export default R.compose(withRouter)(TrackGlobalEvents);
+// Mixpanel reference: https://mixpanel.com/help/reference/http#people-special-properties
+const mixpanelProfileSelector = createSelector(
+  userInfoselectors.countryCodeSelector,
+  userInfoselectors.citySelector,
+  profileSelectors.profileSelector,
+  (countryCode, city, profile) => ({
+    country: countryCode,
+    city,
+    $name: R.path(['profile', 'displayName'], profile),
+    $email: R.prop('email', profile),
+    $phone: R.path(['profile', 'phoneNumber'], profile),
+  }),
+);
+
+const mapStateToProps = createStructuredSelector({
+  isAuthenticated: authSelectors.isAuthenticatedSelector,
+  currentUserId: authSelectors.uidSelector,
+  mixpanelProfile: mixpanelProfileSelector,
+});
+
+export default R.compose(connect(mapStateToProps), withRouter)(
+  TrackGlobalEvents,
+);
