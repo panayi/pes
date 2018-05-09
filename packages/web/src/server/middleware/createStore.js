b5758a54387bcd4f89dd0c466bfa4ff877c03530
Panagiotis Panagi <ppanagi@dynopia.com>
2018-05-09 23:07:42 +0300

    Remove betaUsers fetching

--- packages/web/src/server/middleware/createStore.js
+++ packages/web/src/server/middleware/createStore.js
@@ -1,13 +1,8 @@
 import createMemoryHistory from 'history/createMemoryHistory';
-import * as modelPaths from '@pesposa/core/src/config/modelPaths';
 import configureStore from 'store/configureStore';
 
-const createStore = async (req, res, next) => {
+const createStore = (req, res, next) => {
   const store = configureStore({}, createMemoryHistory());
-
-  await store.firebase.promiseEvents([
-    { path: modelPaths.BETA_USERS.string, type: 'once' },
-  ]);
 
   res.locals.store = store;
   next();
