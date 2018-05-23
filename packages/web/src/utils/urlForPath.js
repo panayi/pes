5ac42f3dfc12accf051a3fa0e46c95feb4b15d7f
Panagiotis Panagi <ppanagi@dynopia.com>
2018-05-23 10:53:17 +0300

    Fix canonical url

--- packages/web/src/utils/urlForPath.js
+++ packages/web/src/utils/urlForPath.js
@@ -2,7 +2,7 @@ import * as R from 'ramda';
 import env from '@pesposa/core/src/config/env';
 
 const urlForPath = R.compose(
-  path => `https://${env.domain}${path}`,
+  path => `https://cy.${env.domain}${path}`,
   R.when(R.compose(R.not, R.equals('/'), R.head), R.concat('/')),
 );
 
