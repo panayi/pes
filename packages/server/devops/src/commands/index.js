40c090fae359ecc6a053f4db1bf103118efb6924
Panagiotis Panagi <ppanagi@dynopia.com>
2018-06-27 18:13:27 +0300

    Add makeAdmin devops operation

--- packages/server/devops/src/commands/index.js
+++ packages/server/devops/src/commands/index.js
@@ -2,5 +2,6 @@ import seed from './seed';
 import importTestData from './importTestData';
 import initialize from './initialize';
 import migrateData from './migrateData';
+import makeAdmin from './makeAdmin';
 
-export { seed, importTestData, initialize, migrateData };
+export { seed, importTestData, initialize, migrateData, makeAdmin };
---
+++ packages/server/devops/src/commands/makeAdmin/index.js
@@ -0,0 +1,44 @@
+import * as admin from 'firebase-admin';
+import logger from 'winston-color';
+import env from '@pesposa/core/src/config/env';
+import firebase from '@pesposa/server-core/src/config/firebaseClient';
+import server from '@pesposa/server-core/src/server';
+
+const makeAdmin = async email => {
+  if (!email) {
+    logger.error('Need to pass an email');
+    process.exit();
+  }
+
+  const emailDomain = email.replace(/.*@/, '');
+  if (emailDomain !== env.internalEmailDomain) {
+    logger.error(
+      `Email domain does not match the internal email domain (${
+        env.internalEmailDomain
+      })`,
+    );
+    process.exit();
+  }
+
+  const userSnap = await server.users.findByEmail(firebase, email);
+  if (!userSnap.exists()) {
+    logger.error(`User with email=${email} does not exist`);
+    process.exit();
+  }
+
+  const userId = userSnap.key;
+  const claims = {
+    admin: true,
+  };
+  await admin.auth().setCustomUserClaims(userId, claims);
+  logger.info('Done!');
+  process.exit();
+};
+
+const command = program =>
+  program
+    .command('makeAdmin <email>')
+    .description('Make a user an admin')
+    .action(makeAdmin);
+
+export default command;
